819484c23b7f20f22893fab8336031fad0aa727d
Puti Hanifah Marsla <putihanifah16@gmail.com>
2024-06-09 11:14:52 +0800

    Menghapus file VerifRps.php

--- app/Models/verifRps.php
+++ app/Models/verifRps.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class verifRps extends Model
+class VerifRps extends Model
 {
-    protected $table = 'verif_rps';
+    protected $table = 'verif_uas';
 
     public $timestamps = false;
 
     protected $fillable = [
-        'id_repo_rps',
+        'id_repo_uas',
         'id_dosen',
-        'status_verif_rps',
+        'status_verif_uas',
         'catatan',
         'tanggal_diverifikasi',
     ];
