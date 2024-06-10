3b5c261c153897544a13edfb1de7433ddd8f963a
fadilaislmns <fadilaislmns99@gmail.com>
2024-06-10 10:50:13 +0700

    sudah

--- app/Models/verifRps.php
+++ app/Models/verifRps.php
@@ -2,21 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VerifRps extends Model
 {
-    protected $table = 'verif_rps';
-
-    public $timestamps = false;
+    protected $table = 'verif_rps'; // Assuming the table name is 'verif_rps'
 
     protected $fillable = [
-        'id_repo_rps',
-        'id_pimpinan_prodi',
-        'id_penguruskbk',
-        'status_verif_rps',
+        'id_verif_rps',
+        'id_dosen',
+        'id_matakuliah',
+        'id_thnakd',
+        'file',
+        'status',
         'catatan',
-        'tanggal_diverifikasi',
+        'tanggal_verif',
     ];
-
+    public $timestamps = false;
 }
